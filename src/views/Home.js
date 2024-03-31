22f785278e6b6d079da0e93bf30cc175a754a056
akshadagith <akshadaanawade@gmail.com>
2024-03-31 12:00:16 +0530

    solve problem in file

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,31 +1,36 @@
-import React, { useState } from 'react'
-import I18n from "../utils/i18n"
+import React, { useState, useEffect } from 'react';
+import I18n from '../utils/i18n';
 
 function Home() {
-    const [lang , setLang] = useState('');
+  const [lang, setLang] = useState('');
+
+  useEffect(() => {
+    const storedLang = localStorage.getItem('lang');
+    if (storedLang) {
+      setLang(storedLang);
+    }
+  }, []);
+
+  const handleLanguageChange = (e) => {
+    const selectedLang = e.target.value;
+    setLang(selectedLang);
+    localStorage.setItem('lang', selectedLang);
+    window.location.reload();
+  };
+
   return (
     <div>
-        <h1>{I18n("welcomeMessage")}</h1>
-        
-        <p>{I18n("normalMessage")}</p>
-
-        <h3>{I18n("greetingMessage")}</h3>
-
-        <select  
-        defaultValue={(e)=>{localStorage.getItem('lang')}}
-        onChange={(e)=>{
-            localStorage.setItem('lang', e.target.value)
-            window.location.reload()
-        }}
-
-        >;
-            <option value="">Select</option>
-            <option value="mr">Marathi</option>
-            <option value="hi">Hindi</option>
-            <option value="en">English</option>
-        </select>
+      <h1>{I18n('welcomeMessage')}</h1>
+      <p>{I18n('normalMessage')}</p>
+      <h3>{I18n('greetingMessage')}</h3>
+      <select value={lang} onChange={handleLanguageChange}>
+        <option value="">Select</option>
+        <option value="mr">Marathi</option>
+        <option value="hi">Hindi</option>
+        <option value="en">English</option>
+      </select>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
